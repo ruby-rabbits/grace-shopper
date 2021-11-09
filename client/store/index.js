213ec42845343970866e11f3508e1e2a6202badb
lrschober <lawrence.schober@gmail.com>
2021-11-09 12:30:49 -0500

    started all users feature

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,11 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import users from './users'
 
-const reducer = combineReducers({ auth })
+
+
+const reducer = combineReducers({ auth }, { users })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -12,3 +15,4 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './auth'
+// export * from './users'
