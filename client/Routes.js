7155cf3ac071f65bccdc3a91952c0696fca607ef
Hyo Kim <hyo.kim968@gmail.com>
2021-11-10 10:14:30 -0800

    changed Home component import to MyAccount import

--- client/Routes.js
+++ client/Routes.js
@@ -3,7 +3,6 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { withRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Login, Signup } from "./components/AuthForm";
-import Home from "./components/Home";
 import LandingPage from "./components/LandingPage";
 import SingleProduct from './components/SingleProduct';
 import MyAccount from './components/MyAccount';
