2ba02f15abf27634b7ff398286981c5496635e85
Hyo Kim <hyo.kim968@gmail.com>
2021-11-13 22:54:12 -0800

    used OMDB api to get movie info + seeded that in database instead with proper column field => works on website

--- script/seedMock.js
+++ script/seedMock.js
@@ -2,17 +2,25 @@
 
 const {
   db,
-  models: { User, Product, Category, Cart_Product},
+  models: { User, Product, Category, Cart_Product },
 } = require('../server/db');
 const fs = require('fs');
-const userData = JSON.parse(fs.readFileSync('mock-data/users.json', 'utf8'))
-const productData = JSON.parse(fs.readFileSync('mock-data/products.json', 'utf8'))
-const cartProductData = JSON.parse(fs.readFileSync('mock-data/cart_product.json', 'utf8'))
+const userData = JSON.parse(fs.readFileSync('mock-data/users.json', 'utf8'));
+// const productData = JSON.parse(
+//   fs.readFileSync('mock-data/products.json', 'utf8')
+// ).slice(0, 100);
+const moviesData = JSON.parse(
+  fs.readFileSync('mock-data/movies.json', 'utf8')
+).slice(0, 100);
+const cartProductData = JSON.parse(
+  fs.readFileSync('mock-data/cart_product.json', 'utf8')
+).slice(0, 100);
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
+
 async function seed() {
   await db.sync({ force: true }); // clears db and matches models to tables
   console.log('db synced!');
@@ -23,25 +31,64 @@ async function seed() {
     User.create({ username: 'murphy', password: '123' }),
   ]);
 
-  const mockUsers = await Promise.all(userData.map(user => User.create({...user, password: '123'})))
+  const mockUsers = await Promise.all(
+    userData.map((user) => User.create({ ...user, password: '123' }))
+  );
 
   const categories = await Promise.all([
-    Category.create({ categoryDisplayName: 'Movies', categoryURLName: 'movies'}),
-    Category.create({ categoryDisplayName: 'Concerts', categoryURLName: 'concerts'}),
-    Category.create({ categoryDisplayName: 'Theatre Shows', categoryURLName: 'theatre'}),
-    Category.create({ categoryDisplayName: 'Sporting Events', categoryURLName: 'sports'}),
-    Category.create({ categoryDisplayName: 'Miscellaneous', categoryURLName: 'misc'})
+    Category.create({
+      categoryDisplayName: 'Movies',
+      categoryURLName: 'movies',
+    }),
+    Category.create({
+      categoryDisplayName: 'Concerts',
+      categoryURLName: 'concerts',
+    }),
+    Category.create({
+      categoryDisplayName: 'Theatre Shows',
+      categoryURLName: 'theatre',
+    }),
+    Category.create({
+      categoryDisplayName: 'Sporting Events',
+      categoryURLName: 'sports',
+    }),
+    Category.create({
+      categoryDisplayName: 'Miscellaneous',
+      categoryURLName: 'misc',
+    }),
   ]);
 
   // Creating products
-  const products = await Promise.all(productData.map(product => Product.create(product)));
+  // const products = await Promise.all(
+  //   productData.map((product) => Product.create(product))
+  // );
+
+  function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+
+  // Adding in movies data from OMDB api
+  const movie_products = await Promise.all(
+    moviesData.map((movie) => {
+      let price = (Math.random()*100).toFixed(2);
+      let categoryId = Math.floor(Math.random() * 5) +1;
+      let date = randomDate(new Date(), new Date(2022, 12, 24))
+      Product.create({price, date, categoryId, picture: movie.Poster, productName: movie.Title})
+    })
+  )
 
   // Creating rows in Cart_Products
-  const cart_products = await Promise.all(cartProductData.map(cp => Cart_Product.create(cp)));
+  const cart_products = await Promise.all(
+    cartProductData.map((cp) => Cart_Product.create(cp))
+  );
 
 
   console.log(
-    `seeded ${users.length + mockUsers.length} users, ${categories.length} categories, ${products.length} products, ${cart_products.length} rows in Cart_Product`
+    `seeded ${users.length + mockUsers.length} users, ${
+      categories.length
+    } categories, ${movie_products.length} products, ${
+      cart_products.length
+    } rows in Cart_Product`
   );
 
   console.log(`seeded successfully`);
@@ -50,11 +97,6 @@ async function seed() {
       cody: users[0],
       murphy: users[1],
     },
-    // products: {
-    //   codewars: products[0],
-    //   dune: products[1],
-    // },
-    // orders
   };
 }
 
