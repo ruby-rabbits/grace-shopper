e49a8942ed895df4328ca6a8f35bf736a7731cc0
Hyo Kim <hyo.kim968@gmail.com>
2021-11-13 23:10:18 -0800

    when deploying, db will be seeded with seedMock

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const { db } = require('./db')
 const PORT = process.env.PORT || 8080
 const app = require('./app')
-const seed = require('../script/seed');
+const seed = require('../script/seedMock');
 
 const init = async () => {
   try {
