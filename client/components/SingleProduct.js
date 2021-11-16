e07fa06a683f12f88ab363af079a8b2ac12ecdf8
Laura Berkey <berkeylaura@gmail.com>
2021-11-16 10:11:17 -0600

    some colors

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -4,42 +4,41 @@ import { Link } from "react-router-dom";
 import { fetchSingleProduct } from "../store/singleProduct";
 
 export class SingleProduct extends React.Component {
-
-  componentDidMount(){
-    this.props.getSingleProduct(this.props.match.params.productId)
+  componentDidMount() {
+    this.props.getSingleProduct(this.props.match.params.productId);
   }
 
-  render(){
-    const product = this.props.product
+  render() {
+    const product = this.props.product;
     // console.log(this.props)
-    return(
+    return (
       <div>
-        <ul>
-          <img src={product.picture}/>
-          <li>{product.productName}</li>
-          <li>{product.description}</li>
-          <li>{product.price}</li>
-        </ul>
-       More stuff can go here! (Maybe an addToCart? Or a quantity they want to purchase in the format of a form/submit?)
-       {/* This is a link back to the home page (All Products) */}
-        <Link to = '/'>
+        <div className="single-product">
+          <img className="single-product" src={product.picture} />
+          <h2 className="single-product">{product.productName}</h2>
+          <div className="single-product">{product.description}</div>
+          <div className="single-product">{product.price}</div>
+        </div>
+        More stuff can go here! (Maybe an addToCart? Or a quantity they want to
+        purchase in the format of a form/submit?)
+        {/* This is a link back to the home page (All Products) */}
+        <Link to="/">
           <h2>Back to All Products</h2>
         </Link>
       </div>
-    )
+    );
   }
 }
 
-const mapState = state => {
-  return{
-    product: state.singleProduct
-  }
-}
-
-const mapDispatch = dispatch => {
+const mapState = (state) => {
   return {
-    getSingleProduct: (id) => dispatch(fetchSingleProduct(id))
-  }
-}
-export default connect(mapState, mapDispatch)(SingleProduct)
+    product: state.singleProduct,
+  };
+};
 
+const mapDispatch = (dispatch) => {
+  return {
+    getSingleProduct: (id) => dispatch(fetchSingleProduct(id)),
+  };
+};
+export default connect(mapState, mapDispatch)(SingleProduct);
